9d920023e577bf5b6b660056609688861c6f7ef3
Radostin Nanov <rn619@york.ac.uk>
2015-02-23 18:03:46 +0000

    Project Contribution
    Currently should work and look not too heinous, but cannot test properly
    given the auth problems.

--- controllers/document.py
+++ controllers/document.py
@@ -13,7 +13,12 @@ def contribute():
     Displays document image and project fields
 
     """
-    return dict()
+    pid = int (request.vars['pid'])
+    project = db(db.project.id==pid).select().first()
+    fields = db(db.field.project==pid).select().first()
+    doc = db(db.doc.project==pid).select().first()
+    form = SQLFORM (db.field, fields)
+    return dict(form=form, doc=doc)
 
 def review():
     """
