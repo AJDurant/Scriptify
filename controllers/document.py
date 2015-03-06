e4e69a53f5c8f3d03a382e6ce2d6f9f1c991b303
Radostin Nanov <rn619@york.ac.uk>
2015-03-06 14:30:32 +0000

    Working on Contributions
    WIP: does not exactly work

--- controllers/document.py
+++ controllers/document.py
@@ -14,10 +14,21 @@ def contribute():
 
     """
     pid = int (request.vars['pid'])
+    docid = int (request.vars['docid'])
     project = db(db.project.id==pid).select().first()
     fields = db(db.field.project==pid).select()
-    doc = db(db.doc.project==pid).select()
-    form = SQLFORM (db.metadata)
+    doc = db(db.doc.id==docid).select().first()
+    contribid=db.contribution.insert (doc=doc.id,contributor=auth.user.id)
+    """
+	Number of problems with this:
+	Dynamic number of forms with dynamic fields referencing dynamically documents. (images)
+	As of now, this functionality isn't programmed.
+	"""
+    form = SQLFORM.factory (db.metadata,
+    Field ("reference",default=doc.id,readable=False,writable=False),
+    Field ("contribution",default=contribid,readable=False,writable=False))
+    if form.accepts (request,session):
+        redirect (URL('document','contribute.html',vars=dict(pid=pid,docid=docid+1)))
     return dict(form=form, project=project, doc=doc, fields=fields)
 
 def review():
